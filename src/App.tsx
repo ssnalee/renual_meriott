a254dd50bfe9bb7f9d015ba2f1dfec85a0c43939
ssnalee <97poolbbang@naver.com>
2024-01-08 19:08:21 +0900

    main header 작업 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import {Routes , Route } from "react-router-dom";
+import styled from "styled-components";
+// import './App.css';
+import { motion, useAnimation , useScroll} from "framer-motion";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Gallery from "./Routes/Gallery";
+// import logo from "./public/image/logo.webp";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element = {<Home />} /> 
+        <Route path="/gallery" element = {<Gallery />} /> 
+      </Routes>
     </div>
   );
 }
